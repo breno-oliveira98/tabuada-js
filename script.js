a391bdc62ce94946f3e0d77362cfd13e07ffa9e6
breno-oliveira98 <breno.oliveira2011@hotmail.com>
2024-07-01 20:31:24 -0300

    alterações finais

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
-for(var i = 0; i < 10; i++) {
-    document.write("Sou a posição " + i)
+function calcularTabela() {
+    let n1 = parseInt(document.getElementById('n1').value);
+    let n2 = parseInt(document.getElementById('n2').value);
+    let operacao = document.getElementById('operacao').value;
+    let resultado = document.getElementById('resultado');
+
+    resultado.innerHTML = "";
+
+    for (let i = 1; i <= n2; i++) {
+        let linha;
+        switch (operacao) {
+            case "soma":
+                linha = `${n1} + ${i} = ${n1 + i}`;
+                break;
+            case "subtracao":
+                linha = `${n1} - ${i} = ${n1 - i}`;
+                break;
+            case "multiplicacao":
+                linha = `${n1} X ${i} = ${n1 * i}`;
+                break;
+            case "divisao":
+                linha = `${n1} ÷ ${i} = ${(n1 / i).toFixed(2)}`;
+                break;
+            default:
+                linha = "Opção inválida, escolhe uma tabuada.";
+        }
+        resultado.innerHTML += linha + "<br>";
+    }
 }
+
+
+function limparCampos() {
+    document.getElementById('n1').value = "";
+    document.getElementById('n2').value = "";
+    document.getElementById('operacao').value = "";
+    document.getElementById('resultado').innerHTML = "";
+}
+
+function verificarInputs() {
+    const n1 = document.getElementById('n1').value.trim();
+    const n2 = document.getElementById('n2').value.trim();
+
+    const calcularButton = document.getElementById('calcular');
+    if (n1 !== "" & n2 !== "") {
+        calcularButton.disabled = false;
+    } else {
+        calcularButton.disabled = true;
+    }
+}
+
+
+document.getElementById('calcular').addEventListener('click', calcularTabela);
+document.getElementById('limpar').addEventListener('click', limparCampos);
+document.getElementById('n1').addEventListener('input', verificarInputs);
+document.getElementById('n2').addEventListener('input', verificarInputs);
